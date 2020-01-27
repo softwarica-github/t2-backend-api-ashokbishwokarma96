e046690457c3d157fe4c3009856a686c608db715
ashokbishwokarma96 <ashokbishwokarma96@gmail.com>
2020-01-27 20:31:28 +0545

    product route added

--- controller/productController.js
+++ controller/productController.js
@@ -4,9 +4,27 @@ const multer = require('multer');
 const Product = require('../model/productModel');
 
 const newProduct = new Product({
-          productName: req.body.name,
-          productCode: req.body.email,
-          productAvailability: hash,
-          productPrice: req.body.phone,
-          materialUsed: req.file.filename,
+          productName: req.body.productName,
+          productCode: req.body.productCode,
+          productAvailability: req.body.productAvailability,
+          productPrice: req.body.productPrice,
+          materialUsed: req.file.materialUsed,
+          productImage: req.file.filename,
+          productQuantity: req.file.productQuantity,
+          productDescription: req.file.productDescription,
+          productMadeDate: req.file.productMadeDate,
+})
+newProduct.save()
+.then(user=>{
+          const token = auth.sign({_id: user._id},process.env.TOKEN);
+          res.status(201).send({
+                    status: "Success",
+                    token: token
+          })
+})
+.catch(err=>{
+          res.status(500).send({
+                    status: "Failure",
+                    error: err
+          })
 })
