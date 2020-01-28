4f8663400bb4840de71637ff1946ef48bdd0dcfb
ashokbishwokarma96 <ashokbishwokarma96@gmail.com>
2020-01-28 14:50:52 +0545

    User update and delete succesful

--- route/userRoute.js
+++ route/userRoute.js
@@ -7,18 +7,22 @@ route.get("/",(req,res)=>{
           res.send("Users route")
 })
 
+// Route for User Signup
 route.route('/signup')
 .get((req,res,next)=>{
           res.send("SIGNUP PAGE")
-})
-.post(upload,userController.signup);
+}).post(upload,userController.signup);
 
+//Route for User Login
 route.route('/login')
 .get((req,res,next)=>{res.send("Login PAGE")})
 .post(userController.login);
 
+//Route for User Update
 route.patch('/update/:_id',upload,userController.update);
+
+//Route for User Delete
 route.delete('/delete/:_id',userController.delete);
 
-
+//Exporting main route of user
 module.exports = route;
